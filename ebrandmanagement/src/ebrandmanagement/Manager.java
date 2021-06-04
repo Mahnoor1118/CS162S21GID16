4c58a5a9671e2a948ef0294242f55a844408be74
Mahnoor1118 <81877832+Mahnoor1118@users.noreply.github.com>
2021-06-04 01:11:44 -0700

    classes
    main classes

---
+++ ebrandmanagement/src/ebrandmanagement/Manager.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ebrandmanagement;
+
+/**
+ *
+ * @author MAHNOOR SHAD
+ */
+public class Manager extends Person {
+    private String username;
+    private String password;
+    private String qualification;
+    private String categoury;
+    private String salary;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public String getCategoury() {
+        return categoury;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+    public void setCategoury(String categoury) {
+        this.categoury = categoury;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+    
+    
+    
+}
