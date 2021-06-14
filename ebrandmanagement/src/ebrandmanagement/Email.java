566d7670349b740ec2b2e540c9919fb06afbdb8d
Mahnoor1118 <81877832+Mahnoor1118@users.noreply.github.com>
2021-06-14 10:49:50 -0700

    email
    code

---
+++ ebrandmanagement/src/ebrandmanagement/Email.java
@@ -0,0 +1,119 @@
+package ebrandmanagement;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage; 
+
+/**
+ *
+ * @author Haider
+ */
+
+public class Email {
+    private String sender;
+    private String receiver;
+    private String message;
+    private String subject;
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+    
+    
+    /**
+     * function used to send email
+     */
+    public void sendEmail(){
+       
+        
+        // Recipient's email ID needs to be mentioned.
+        String to = "receiverEmail.com";
+
+        // Sender's email ID needs to be mentioned
+        String from = "senderEmail.com";
+
+        // Assuming you are sending email from through gmails smtp
+        String host = "smtp.gmail.com";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+        // Get the Session object.// and pass username and password
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+
+                return new PasswordAuthentication("senderEmail.com", "password of sender");
+
+            }
+
+        });
+
+        // Used to debug SMTP issues
+        session.setDebug(true);
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject("scholarship holder");
+
+            // Now set the actual message
+            message.setText("panga pila");
+
+            System.out.println("sending...");
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+        }
+
+    }
+    
+}
